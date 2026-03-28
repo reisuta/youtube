ac3045b5c0563dd6eac00350194cf6a90f17b3c6
reisuta <in345vain@gmail.com>
2026-03-28 09:58:17 +0900

    【Vim中級者必見】マクロで繰り返し作業を完全自動化する

---
+++ vim/macro/samples/part4_fetch_methods.rb
@@ -0,0 +1,8 @@
+def fetch_user(id)
+end
+
+def fetch_product(id)
+end
+
+def sync_helper    # このメソッドはコメント不要
+end
---
+++ vim/macro/samples/part6_calculate_methods.rb
@@ -0,0 +1,3 @@
+def calculate_tax(amount)
+def calculate_discount(amount)
+def calculate_total(amount)
